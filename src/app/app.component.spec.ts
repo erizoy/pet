a54a2a420ec2bb7c4888752a323c20a6b7abde68
nikolay.filimonov <erizoy@gmail.com>
2021-05-13 13:21:10 +0300

    update project name

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'shopping-list'`, () => {
+  it(`should have as title 'pet'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('shopping-list');
+    expect(app.title).toEqual('pet');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('shopping-list app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('pet app is running!');
   });
 });
